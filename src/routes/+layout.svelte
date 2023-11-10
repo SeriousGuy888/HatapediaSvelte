1f553197de1c2934ecf1c6049705ab7f3a4125fe
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-10 18:09:53 -0500

    dark mode light mode colour fade transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,23 @@
 
 <div class="layout">
   <Header />
-  <main class="dark:bg-black">
+  <main>
     <slot />
   </main>
 </div>
 
-<style>
+<style lang="postcss">
   .layout {
     display: grid;
     grid-template-rows: auto 1fr;
     min-height: 100vh;
+  }
+
+  .layout {
+    @apply transition-colors duration-300 ease-in-out;
+  }
+
+  :global(body.dark) .layout {
+    @apply text-white bg-black;
   }
 </style>
