92147edac12c149ba2fcccb2732d7a0ea256f6b8
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-11 14:54:02 -0500

    render article tags on page

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import ArticleTag from './ArticleTag.svelte'
+
   export let data
 </script>
 
@@ -8,31 +10,43 @@
   <meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<div class="article-container">
-  <hgroup>
-    <h1>{data.meta.title}</h1>
-    {#if data.meta.subtitle}
-      <h2>{data.meta.subtitle}</h2>
-    {/if}
-  </hgroup>
+<article class="article-container">
+  <header>
+    <hgroup>
+      <h1>{data.meta.title}</h1>
+      {#if data.meta.subtitle}
+        <p>{data.meta.subtitle}</p>
+      {/if}
+    </hgroup>
+    <div class="tag-list">
+      {#each data.meta.tags as tag}
+        <ArticleTag tag={tag} />
+      {/each}
+    </div>
+  </header>
 
-  <article class="prose dark:prose-invert">
+  <div class="prose dark:prose-invert">
     <svelte:component this={data.content} />
-  </article>
-</div>
+  </div>
+</article>
 
 <style lang="postcss">
-  hgroup {
-    @apply border-b-2 border-gray-200 dark:border-gray-700 pb-4 mb-8;
+  header {
+    @apply border-b-2 border-gray-200 dark:border-gray-700 pb-8 mb-8;
   }
+
   hgroup h1 {
     @apply text-4xl sm:text-5xl font-bold mb-2 break-words;
   }
-  hgroup h2 {
+  hgroup p {
     @apply text-gray-600 dark:text-gray-300 uppercase text-sm break-words;
   }
-  
+
   .article-container {
     @apply max-w-prose mx-auto px-4 my-8;
+  }
+
+  .tag-list {
+    @apply flex flex-wrap gap-2 mt-4;
   }
 </style>
---
+++ src/routes/articles/[slug]/ArticleTag.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let tag: string
+</script>
+
+<a href={`/tags/${tag}`} aria-label={`Tag: ${tag}`}>
+  <span
+    class={`
+    text-sm font-mono
+    px-2 py-1 rounded-full border-2
+    text-black border-purple-300 bg-purple-300 hover:border-purple-600
+    dark:text-white dark:border-purple-900 dark:bg-purple-900 dark:hover:border-purple-500`}
+  >
+    #{tag}
+  </span>
+</a>
