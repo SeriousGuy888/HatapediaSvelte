6aac71d23a13b6395fde40717d9ca9b898c2c0d0
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-11 18:18:18 -0500

    remove unused code

--- src/routes/tags/[tag]/+page.server.ts
+++ src/routes/tags/[tag]/+page.server.ts
@@ -1,11 +1,7 @@
 import { getArticles } from "$lib/articles"
-import type { Article } from "$lib/types"
 
 export async function load({ fetch, params }) {
   const tag = params.tag
-
-  // const response = await fetch(`/api/articles/tags/${tag}`)
-  // const articles: Article[] = await response.json()
 
   const articles = await getArticles()
   const filteredArticles = articles.filter((article) => article.tags?.includes(tag))
