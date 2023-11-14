934aa69254ddf48115ce27375fd3832d1339b861
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-13 20:48:00 -0500

    wip header search bar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,13 @@
 <script>
+    import HeaderSearchBar from "./HeaderSearchBar.svelte"
   import Logo from "./Logo.svelte"
   import ThemeToggle from "./ThemeToggle.svelte"
 </script>
 
-<nav>
+<nav class="flex justify-between items-center py-4 px-8 gap-8 bg-blue-900">
   <Logo />
-  <ThemeToggle />
+  <div class="flex gap-4">
+    <HeaderSearchBar />
+    <ThemeToggle />
+  </div>
 </nav>
-
-<style lang="postcss">
-  nav {
-    @apply flex justify-between items-center py-4 px-8 gap-8 bg-blue-900;
-  }
-</style>
---
+++ src/routes/HeaderSearchBar.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Search } from "lucide-svelte"
+
+  let searchModalOpen = false
+  function openSearchModal() {}
+</script>
+
+<button on:click={openSearchModal} aria-label="Open the search modal">
+  <Search size="24" />
+</button>
+
+<!-- Search Modal -->
+<div class={`${searchModalOpen ? "block" : "hidden"}`}>
+  <div class="fixed inset-0 bg-black bg-opacity-50" />
+  <div class="fixed inset-0 flex justify-center items-center">
+    <div class="bg-white dark:bg-gray-800 rounded-lg p-8">
+      <div class="relative w-full sm:max-w-3xl">
+        <input
+          type="text"
+          class="appearance-none bg-transparent p-2 focus:outline-none border-b-2 border-blue-600 dark:border-blue-400 w-full sm:text-xl"
+          placeholder="Search for an article..."
+        />
+        <Search class="absolute right-2 top-2 w-6 h-6 text-blue-600 dark:text-blue-400" />
+      </div>
+    </div>
+  </div>
+</div>
