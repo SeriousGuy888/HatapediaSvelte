bb9651bd78eb6d82bc913785b40b552b0e73a85f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-17 14:58:40 -0500

    add link to map in header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,22 @@
 <script>
-    import HeaderSearchBar from "./HeaderQuickSwitcher.svelte"
+  import HeaderSearchBar from "./HeaderQuickSwitcher.svelte"
   import Logo from "./Logo.svelte"
   import ThemeToggle from "./HeaderThemeToggle.svelte"
 </script>
 
 <nav class="flex justify-between items-center py-4 px-8 gap-8 bg-blue-900">
-  <Logo />
-  <div class="flex gap-4">
+  <section class="text-sm text-white">
+    <Logo />
+    <a href="/map">Map</a>
+  </section>
+  <section>
     <HeaderSearchBar />
     <ThemeToggle />
-  </div>
+  </section>
 </nav>
+
+<style lang="postcss">
+  section {
+    @apply flex gap-4 items-center;
+  }
+</style>
