962e22c0860e5f5a1d7b8aa724a93f3a38074a70
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-17 11:51:48 -0500

    rename theme toggle to be more consistent

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <script>
     import HeaderSearchBar from "./HeaderQuickSwitcher.svelte"
   import Logo from "./Logo.svelte"
-  import ThemeToggle from "./ThemeToggle.svelte"
+  import ThemeToggle from "./HeaderThemeToggle.svelte"
 </script>
 
 <nav class="flex justify-between items-center py-4 px-8 gap-8 bg-blue-900">
