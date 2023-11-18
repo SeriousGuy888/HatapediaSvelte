3e42276f610d004b487c0df4c9133457d0682a6b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-17 21:11:14 -0500

    remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
 import adapter from "@sveltejs/adapter-auto"
 import { vitePreprocess } from "@sveltejs/kit/vite"
-import { mdsvex } from "mdsvex"
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-  extensions: [".md"],
-}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
-  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter(),
   },
