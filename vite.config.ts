76cc3b2434020e044032ccf9c3fa0aa97cb7b56d
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-11-17 21:12:25 -0500

    vite plugin to allow dynamic importing from strings with variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 
+// https://stackoverflow.com/a/71350674/18947288
+import dynamicImportVars from "@rollup/plugin-dynamic-import-vars"
+
 // import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-  ],
+  plugins: [sveltekit(), dynamicImportVars({ include: ["**/*.md"] })],
   // optimizeDeps: {
   //   // https://github.com/jonschlinkert/gray-matter/issues/143#issuecomment-1119218579
   //   esbuildOptions: {
