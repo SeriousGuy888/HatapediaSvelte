11dd1a50b08fd112e6d8d50a3ad2fbf61598e63a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-12-05 14:28:59 -0500

    stole some code to dynamically load svelte components

---
+++ src/lib/dynamic_components/DynamicComponentWrapper.svelte
@@ -0,0 +1,13 @@
+<!--
+  Stolen from
+  https://github.com/dimfeld/website/blob/54c30d47ecaa02fabfc5c5ccedf419035da02c77/src/DynamicComponentWrapper.svelte
+-->
+
+<script>
+  export let component
+  export let props
+</script>
+
+<div>
+  <svelte:component this={component} {...props} />
+</div>
---
+++ src/lib/dynamic_components/dynamic_components_in_md.ts
@@ -0,0 +1,90 @@
+// Largely stolen from
+// https://github.com/dimfeld/website/blob/54c30d47ecaa02fabfc5c5ccedf419035da02c77/src/dynamicComponents.ts
+
+import { tick, type SvelteComponent } from "svelte"
+import DynamicComponentWrapper from "./DynamicComponentWrapper.svelte"
+
+const components: Record<string, () => Promise<any>> = {
+  CharacterInfobox: () => import("$lib/components/article_elements/CharacterInfobox.svelte"),
+}
+
+async function instantiateComponent(element: Element) {
+  try {
+    // Get the attributes of the element <div attribute-name="value">
+    const attrNames = element.getAttributeNames()
+
+    // Store here the Svelte component to be rendered in its place if found
+    let component: SvelteComponent | null = null
+    const props: Record<string, any> = {}
+
+    // Loop through all the attributes of the element
+    for (const attrName of attrNames) {
+      const value = element.getAttribute(attrName)
+      if (!value) {
+        continue
+      }
+
+      if (attrName === "data-component") {
+        const componentImporter = components[value]
+
+        if (!componentImporter) {
+          console.warn(`Dynamic component ${value} not defined.`)
+          continue
+        }
+
+        console.log("Loading dynamic component", value)
+        await tick() // Wait for Svelte to tick
+        component = (await componentImporter()).default
+        console.log("Loaded dynamic component", value)
+      } else if (attrName.startsWith("data-prop-")) {
+        // Remove the "data-prop-" prefix from the attribute name
+        const propName = attrName.slice("data-prop".length + 1)
+
+        // Store the string value of the attribute as a prop
+        props[propName] = value
+      }
+    }
+
+    if (!component) {
+      return
+    }
+
+    console.log("Instantiating dynamic component", component.name, "in place of", element)
+
+    const instance = new DynamicComponentWrapper({
+      target: element,
+      props: {
+        component,
+        props,
+      },
+    })
+
+    element.classList.add("has-component")
+
+    return instance
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+export async function instantiateDynamicComponents() {
+  const divs = document.querySelectorAll("[data-component]")
+  const components: SvelteComponent[] = []
+
+  for (const div of divs) {
+    const component = await instantiateComponent(div)
+    if (component) {
+      components.push(component)
+    }
+  }
+
+  return () => {
+    for (const component of components) {
+      try {
+        component.$destroy()
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+}
