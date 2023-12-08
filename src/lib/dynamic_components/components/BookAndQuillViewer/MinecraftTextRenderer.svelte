dcd25c21459283f8d4783e54b12c542117e02770
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-12-08 17:31:18 -0500

    add proper colour map to book and quill renderer

--- src/lib/dynamic_components/components/BookAndQuillViewer/MinecraftTextRenderer.svelte
+++ src/lib/dynamic_components/components/BookAndQuillViewer/MinecraftTextRenderer.svelte
@@ -26,13 +26,55 @@
 
     console.log(flattenedExtras)
   }
+
+  function mcColourToHex(mcCol: string) {
+    // https://minecraft.wiki/w/Formatting_codes#Color_codes
+    switch (mcCol) {
+      case "black":
+      case "reset":
+        return "#000000"
+      case "dark_blue":
+        return "#0000AA"
+      case "dark_green":
+        return "#00AA00"
+      case "dark_aqua":
+        return "#00AAAA"
+      case "dark_red":
+        return "#AA0000"
+      case "dark_purple":
+        return "#AA00AA"
+      case "gold":
+        return "#FFAA00"
+      case "gray":
+        return "#AAAAAA"
+      case "dark_gray":
+        return "#555555"
+      case "blue":
+        return "#5555FF"
+      case "green":
+        return "#55FF55"
+      case "aqua":
+        return "#55FFFF"
+      case "red":
+        return "#FF5555"
+      case "light_purple":
+        return "#FF55FF"
+      case "yellow":
+        return "#FFFF55"
+      case "white":
+        return "#FFFFFF"
+      default:
+        return mcCol
+    }
+  }
 </script>
 
 {#each flattenedExtras as textComponent}
   {@const lines = textComponent.text?.split("\\n") ?? []}
   <p
+    class="text-minecraft"
     style={`
-      color: ${textComponent.color?.replace(/_/g, "") ?? "black"};
+      color: ${mcColourToHex(textComponent.color ?? "black")};
       font-weight: ${textComponent.bold ? "bold" : "normal"};
       font-style: ${textComponent.italic ? "italic" : "normal"};
       text-decoration: ${textComponent.underlined ? "underline" : "none"};
