c83f4f1bbff3fcacb4d43896519804e9b40c22ca
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-12-08 22:28:11 -0500

    soft-navigation progressive enhancement on article link cards
    also removed the illegal nested <a> tags

--- src/lib/components/ArticleLinkCard.svelte
+++ src/lib/components/ArticleLinkCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { Article } from "$lib/types"
   import ArticleTag from "$lib/components/ArticleTag.svelte"
-  // import { goto } from "$app/navigation"
+  import { goto } from "$app/navigation"
   import { createEventDispatcher } from "svelte"
 
   export let article: Article
@@ -9,7 +9,7 @@
 
   const dispatch = createEventDispatcher()
 
-  let elem: HTMLAnchorElement | null = null
+  let elem: HTMLDivElement | null = null
 
   $: {
     if (selected) {
@@ -21,48 +21,84 @@
       elem?.focus()
     }
   }
+
+  let target = `/articles/${article.slug}`
+  function goThere() {
+    goto(target)
+    dispatch("navigate")
+  }
 </script>
 
-<!-- TODO: Make this not render broken when JS is disabled -->
-
-<a
-  href="/articles/{article.slug}"
+<div
   class:selected
   bind:this={elem}
-  on:click={() => {
-    // This sometimes makes the link go to the wrong place for some reason
-    // goto(`/articles/${article.slug}`)
-    dispatch("navigate")
+  on:click={goThere}
+  on:keydown={(e) => {
+    if (e.key === "Enter") goThere()
   }}
+  class="card relative isolate"
+  role="link"
+  tabindex="0"
 >
-  <span>
-    <h2 class="title dotdotdot">
-      {article.title}
-    </h2>
-    <p class="subtitle dotdotdot">
-      {article.subtitle}
-    </p>
-    <span class="tag-list">
+  <!--
+    This <a> is here because it's illegal to have an <a> inside another <a>, and
+    it will render incorrectly if JavaScript is disabled. (ArticleLink has an
+    <a> inside it, so it's illegal to put it inside another <a>.)
+
+    https://stackoverflow.com/a/13054959
+
+    In this workaround, the card itself is just a div with a click event and
+    uses JS to soft-navigate to the target URL. The <a> is absolutely positioned
+    on top of the card and is always invisible.
+    
+    If JavaScript is enabled, clicking on the <a> will preventDefault, and the
+    card's click event will handle the navigation. If JavaScript is disabled,
+    the card's click event will not work, and neither will the preventDefault,
+    so the <a> will handle the navigation.
+
+    As a result, the card will always be clickable, whether JS is enabled or not,
+    just with a slightly better experience if JS is enabled, and the card will
+    still render correctly if JS is disabled.
+  -->
+  <a
+    href={target}
+    on:click|preventDefault
+    class="absolute inset-0 z-10 text-transparent select-none"
+  >
+    Link to {article.title}
+  </a>
+  <h2 class="title dotdotdot">
+    {article.title}
+  </h2>
+  <p class="subtitle dotdotdot">
+    {article.subtitle}
+  </p>
+  <!--
+    Render tags on top of the <a> tag that covers everything else so that you
+    can still click on the tags.
+  -->
+  <div class="mt-4 h-5 relative">
+    <div class="flex flex-nowrap gap-2 absolute z-20 max-w-full overflow-auto no-scrollbar">
       {#each article.tags as tag}
         <ArticleTag {tag} size="small" />
       {/each}
-    </span>
-  </span>
-</a>
+    </div>
+  </div>
+</div>
 
 <style lang="postcss">
-  a {
+  .card {
     @apply p-4 rounded-lg;
     @apply bg-gray-100 dark:bg-gray-900;
     @apply text-black dark:text-white;
     @apply shadow-md shadow-gray-300 dark:shadow-gray-700;
     @apply border-t-8 border-transparent;
-    @apply outline-none;
     @apply transition-colors duration-100 ease-in-out;
+    @apply outline-none cursor-pointer;
   }
 
-  a:hover,
-  a:focus,
+  .card:hover,
+  .card:focus,
   .selected {
     @apply border-t-primary;
   }
@@ -73,10 +109,6 @@
 
   .subtitle {
     @apply text-xs opacity-75 uppercase;
-  }
-
-  .tag-list {
-    @apply flex flex-wrap gap-2 mt-4;
   }
 
   .dotdotdot {
