a6032518d0a2fa89f66de3bb8249bc2d10579820
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-12-08 23:54:08 -0500

    fixed table of contents scrollability

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -68,7 +68,7 @@
       bg-opacity-20 bg-gray-200 dark:bg-opacity-30 dark:bg-gray-900
     `}
   >
-    <div class="sticky top-16 px-2 pt-4 max-h-full overflow-y-auto">
+    <div class="sticky top-16 px-2 py-4 max-h-[calc(100vh-4rem)] overflow-y-auto">
       <p class="text-center font-bold">Contents</p>
       <div class="-ml-3">
         <TableOfContents headings={[topLink, ...headings]} />
