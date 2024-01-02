33a7982a0693f49db857f8c6734e952571e25e42
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-01-02 00:38:31 -0500

    wqhy is tpyojescript so hard tjo make work

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto"
+import vercel from "@sveltejs/adapter-vercel"
 import { vitePreprocess } from "@sveltejs/kit/vite"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,7 @@ const config = {
   extensions: [".svelte", ".md"],
   preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     prerender: {
       // Stops the build from failing because of dangling links in articles
       // since there are a lot of links to non-existent articles
