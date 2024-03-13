e055a1225692d7873b8cb403dbe0609297228d6c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-13 16:53:24 -0400

    feat: add vercel analytics & speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,12 @@
 import { allArticleMeta } from "$lib/stores/article_store.js"
 import type { Article } from "$lib/types"
+import { dev } from "$app/environment"
+
+import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+
+inject({ mode: dev ? "development" : "production" })
+injectSpeedInsights()
 
 export async function load({ fetch }) {
   const response = await fetch("/api/articles")
