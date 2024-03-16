36d18fca58fba5f19a50cc6fc2657fdde41e50b6
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-16 15:55:07 -0400

    fix: youtube embeds no longer cause horizontal overflow on mobile

--- src/lib/dynamic_components/components/YouTubeVideo.svelte
+++ src/lib/dynamic_components/components/YouTubeVideo.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
   export let video: string
-
-  const width = 560
-  const height = 315
 </script>
 
 {#if video}
   <iframe
-    {width}
-    {height}
     src="https://www.youtube-nocookie.com/embed/{video}"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen
+    class="w-full aspect-video"
   />
 {:else}
   <div
-    class="bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded text-center grid place-items-center"
-    style="width: {width}px; height: {height}px;"
+    class="bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded text-center grid place-items-center w-full h-full"
   >
     <div>
       <p class="font-bold">Invalid YouTube video specified.</p>
