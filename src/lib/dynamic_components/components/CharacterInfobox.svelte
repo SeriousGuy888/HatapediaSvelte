0dd6737a9c23f2f0061b2e1e5cda95d252f86386
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-18 10:39:11 -0400

    feat: use 3d minecraft skin api for character infoboxes

--- src/lib/dynamic_components/components/CharacterInfobox.svelte
+++ src/lib/dynamic_components/components/CharacterInfobox.svelte
@@ -29,7 +29,7 @@
           />
         {:else if mcUuid}
           <img
-            src="https://minotar.net/armor/body/{mcUuid}/16.png"
+            src="https://visage.surgeplay.com/full/256/{mcUuid}.png?no=shadow&y=-40"
             alt="Minecraft skin"
             loading="lazy"
             width="16"
