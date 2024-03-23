53c2f1815fbddbda7517521dab7e46949f2e22e1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-23 00:55:06 -0400

    added title & description to map page

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,1 +1,15 @@
-<iframe src="/map/unmined.index.html" title="HATA 2 World Map" width="100%" height="100%" />
+<script>
+  const lastUpdated = "2024-03-23"
+  const title = "HATA 2 World Map"
+</script>
+
+<svelte:head>
+  <title>{title} ({lastUpdated})</title>
+  <meta property="og:title" content={title} />
+  <meta
+    property="og:description"
+    content="An interactive map of the HATA 2 overworld. Last updated {lastUpdated}"
+  />
+</svelte:head>
+
+<iframe src="/map/unmined.index.html" {title} width="100%" height="100%" />
