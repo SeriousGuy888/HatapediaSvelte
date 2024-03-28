7031ec944da0f105ec141f6a1c7e4e31e24c4846
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-28 19:35:29 -0400

    wait no

--- utils/sluggify.js
+++ utils/sluggify.js
@@ -6,8 +6,8 @@ export function sluggify(fileName) {
   return fileName
     .toLowerCase()
     .replace(/\.md$/, "") // Remove .md file extension
-    .replace(/\s+/g, "_") // Replace spaces with underscores
-    .replace(/[^a-z0-9_-]/g, "") // Remove anything not alphanumeric, underscore, or hyphen
     .normalize("NFD") // Split an accented character into the base character and the diacritics
     .replace(/\p{Diacritic}/gu, "") // Remove diacritics. e.g. café -> cafe
+    .replace(/\s+/g, "_") // Replace spaces with underscores
+    .replace(/[^a-z0-9_-]/g, "") // Remove anything not alphanumeric, underscore, or hyphen
 }
