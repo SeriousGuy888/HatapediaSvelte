bf1521d1e7ff748cc7dc223c15a872e940acf389
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-29 19:44:11 -0400

    obsidian workspace and stuff

--- static/map/custom.markers.js
+++ static/map/custom.markers.js
@@ -37,50 +37,17 @@ Use the imageScale property if the pin image is too large.
 */
 
 UnminedCustomMarkers = {
+  isEnabled: true,
 
-    isEnabled: false,
-
-    markers: [
-
-        // Example 1: Simple marker
-        {
-            x: -200,
-            z: -200,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-        },
-
-        // Example 2: Marker with text
-        {
-            x: 0,
-            z: 0,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-            text: "Marker with text",
-            textColor: "red", 
-            offsetX: 0,
-            offsetY: 20,
-            font: "bold 20px Calibri,sans serif",
-        },
-
-        // Example 3: Text only
-        {
-            x: 200,
-            z: 200,
-            text: "Text only", 
-            textColor: "yellow", 
-            offsetX: 0,
-            offsetY: 0, 
-            font: "bold 50px Calibri,sans serif",
-        },
-
-        // add your markers here
-
-
-
-
-        // do not delete the following two closing brackets
-    ]
+  markers: [
+    // Example 1: Simple marker
+    {
+      x: 1850,
+      z: -1460,
+      image:
+        "https://www.mapmarker.io/api/v3/font-awesome/v6/icon?size=25&icon=fa-solid%20fa-skull-crossbones&color=ffffff",
+      imageAnchor: [0.5, 1],
+      imageScale: 1,
+    },
+  ],
 }
