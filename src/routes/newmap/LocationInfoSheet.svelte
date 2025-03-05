2bcb1ec0b44ee63e8fb4f48ef2c5441278604e7f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-03-05 00:09:33 -0500

    allow swiping to toggle info sheet

--- src/routes/newmap/LocationInfoSheet.svelte
+++ src/routes/newmap/LocationInfoSheet.svelte
@@ -7,6 +7,9 @@
     location: MapLocation | null
   }
   let { expanded, location }: Props = $props()
+
+  let isSwiping = $state(false)
+  let initialSwipePos = $state([0, 0])
 </script>
 
 <div class="fixed bottom-0 inset-x-1 pointer-events-none">
@@ -22,10 +25,34 @@
     <button
       class="w-full px-4 py-2 cursor-pointer bg-background border-2 border-b-0 border-foreground rounded-t-xl transition-transform flex gap-2 align-middle"
       style:translate={expanded ? "0% 0%" : "0% -100%"}
+      aria-label={expanded ? "Hide sheet" : "Show sheet"}
+      onpointerdown={(event) => {
+        isSwiping = true
+        initialSwipePos = [event.clientX, event.clientY]
+      }}
+      onpointerup={() => {
+        isSwiping = false
+        initialSwipePos = [0, 0]
+      }}
+      onpointermove={(event) => {
+        if (!isSwiping) {
+          return
+        }
+
+        event.preventDefault()
+
+        const dY = event.clientY - initialSwipePos[1]
+        if (dY > 5) {
+          // swiped down
+          expanded = false
+        } else if (dY < -5) {
+          // swiped up
+          expanded = true
+        }
+      }}
       onclick={(event) => {
         expanded = !expanded
       }}
-      aria-label={expanded ? "Hide sheet" : "Show sheet"}
     >
       <h2 class="grow text-left font-bold">
         {location ? location.name : "Nothing selected."}
