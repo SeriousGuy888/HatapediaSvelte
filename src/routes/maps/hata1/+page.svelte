a0880a677e7be702e206e93f44efa82704e88f4a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-03-07 23:51:43 -0500

    move coordinate conversion functions to separate file

--- src/routes/maps/hata1/+page.svelte
+++ src/routes/maps/hata1/+page.svelte
@@ -13,6 +13,12 @@
 
   import { MAP_DIMENSIONS, MAP_WORLD_ORIGIN_OFFSET, WORLD_DEFAULT_LOCATION } from "./map_config"
   import { cameraState, changeZoom, frameState } from "./view_state.svelte"
+  import {
+    clientSpaceToScreenSpace,
+    imageSpaceToWorldSpace,
+    screenSpaceToImageSpace,
+    worldSpaceToImageSpace,
+  } from "./coordinates.svelte"
 
   let mapCamera: HTMLDivElement // Contains map frame, but hides overflow, only showing a part of the map frame.
   let mapFrame: HTMLDivElement // Holds the map image, and is transformed around with CSS to zoom and pan.
@@ -57,30 +63,6 @@
 
   function updateMousePos(event: MouseEvent) {
     ;[mouseScreenX, mouseScreenY] = clientSpaceToScreenSpace(event.clientX, event.clientY)
-  }
-
-  /**
-   * "Clientspace" is the position in the window, with the top left corner of the webpage being 0, 0.
-   * "Screenspace" is the position in the view of the camera, with the top left corner of the visible part
-   * of the map being 0, 0.
-   */
-  function clientSpaceToScreenSpace(clientX: number, clientY: number): [number, number] {
-    return [clientX - cameraState.left, clientY - cameraState.top]
-  }
-
-  function screenSpaceToImageSpace(screenX: number, screenY: number): [number, number] {
-    return [
-      ~~((screenX + frameState.offsetX) / cameraState.zoom),
-      ~~((screenY + frameState.offsetY) / cameraState.zoom),
-    ]
-  }
-
-  function imageSpaceToWorldSpace(imageX: number, imageY: number): [number, number] {
-    return [imageX - MAP_WORLD_ORIGIN_OFFSET[0], imageY - MAP_WORLD_ORIGIN_OFFSET[1]]
-  }
-
-  function worldSpaceToImageSpace(worldX: number, worldY: number): [number, number] {
-    return [worldX + MAP_WORLD_ORIGIN_OFFSET[0], worldY + MAP_WORLD_ORIGIN_OFFSET[1]]
   }
 
   function doDrag(x: number, y: number) {
---
+++ src/routes/maps/hata1/coordinates.svelte.ts
@@ -0,0 +1,25 @@
+import { MAP_WORLD_ORIGIN_OFFSET } from "./map_config"
+import { cameraState, frameState } from "./view_state.svelte"
+/**
+ * "Clientspace" is the position in the window, with the top left corner of the webpage being 0, 0.
+ * "Screenspace" is the position in the view of the camera, with the top left corner of the visible part
+ * of the map being 0, 0.
+ */
+export function clientSpaceToScreenSpace(clientX: number, clientY: number): [number, number] {
+  return [clientX - cameraState.left, clientY - cameraState.top]
+}
+
+export function screenSpaceToImageSpace(screenX: number, screenY: number): [number, number] {
+  return [
+    ~~((screenX + frameState.offsetX) / cameraState.zoom),
+    ~~((screenY + frameState.offsetY) / cameraState.zoom),
+  ]
+}
+
+export function imageSpaceToWorldSpace(imageX: number, imageY: number): [number, number] {
+  return [imageX - MAP_WORLD_ORIGIN_OFFSET[0], imageY - MAP_WORLD_ORIGIN_OFFSET[1]]
+}
+
+export function worldSpaceToImageSpace(worldX: number, worldY: number): [number, number] {
+  return [worldX + MAP_WORLD_ORIGIN_OFFSET[0], worldY + MAP_WORLD_ORIGIN_OFFSET[1]]
+}
